47ecf29a590658dae08df90d329d623e1e1c64bd
KoblizekXD <janprokupek@centrum.cz>
2024-03-12 07:27:00 +0100

    Refactor the use of ValueOf

--- src/main/java/lol/koblizek/craftorm/test/TestMain.java
+++ src/main/java/lol/koblizek/craftorm/test/TestMain.java
@@ -3,13 +3,18 @@ package lol.koblizek.craftorm.test;
 import lol.koblizek.craftorm.CraftORM;
 import lol.koblizek.craftorm.ResourceProvider;
 import lol.koblizek.craftorm.beans.Inject;
+import lol.koblizek.craftorm.util.properties.ValueOf;
 
 public class TestMain {
 
     @Inject
     private static ResourceProvider resourceProvider;
 
+    @ValueOf("database.url")
+    private static String url;
+
     public static void main(String[] args) {
         CraftORM.start(TestMain.class);
+        System.out.println(url);
     }
 }
--- src/main/java/lol/koblizek/craftorm/util/properties/ValueOf.java
+++ src/main/java/lol/koblizek/craftorm/util/properties/ValueOf.java
@@ -1,5 +1,6 @@
 package lol.koblizek.craftorm.util.properties;
 
+import lol.koblizek.craftorm.ResourceProvider;
 import lol.koblizek.craftorm.beans.BeanLoader;
 import lol.koblizek.craftorm.beans.WithProcessor;
 import lol.koblizek.craftorm.util.BeanDiscoverable;
@@ -9,18 +10,24 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.lang.reflect.Member;
+import java.util.Properties;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 @WithProcessor(ValueOf.ValueOfProcessor.class)
 public @interface ValueOf {
     String value();
+    String resource() default "database.properties";
 
     class ValueOfProcessor implements BeanDiscoverable<ValueOf> {
 
         @Override
         public Object onApplied(BeanLoader beanLoader, ValueOf annotation, ElementType element, Member member) {
-            return annotation.value();
+            Properties properties = beanLoader.getBean(ResourceProvider.class).getPropertyFile(annotation.resource());
+            String property = properties.getProperty(annotation.value());
+            if (property == null)
+                throw new IllegalArgumentException("Property " + annotation.value() + " not found in " + annotation.resource() + " file.");
+            return property;
         }
     }
 }
